dc1b3ab1990041ec9e2b700e2156c4d76acff87c
nons9633 <novall9633@naver.com>
2023-09-01 15:00:00 +0900

    smooth scroll

---
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/JS/main.js
@@ -0,0 +1,11 @@
+// 도깨비 PJ 메인 JS - main.js
+
+// 로딩구역 호출설정
+window.addEventListener('DOMContentLoaded',loadFn);
+
+// 로딩구역 함수////////////////////////
+function loadFn(){
+    // 호출확인
+    console.log('로딩완료');
+    startSS();
+};///////////////loadFn함수///////////////////
