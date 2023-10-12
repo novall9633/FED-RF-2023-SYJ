2efd55ef8c11a17b6fb3923b5db6e5cb63089fd2
nons9633 <novall9633@naver.com>
2023-10-12 17:33:56 +0900

    10/12

--- 003.JS학습/06.JS달력/js/calendar.js
+++ 003.JS학습/06.JS달력/js/calendar.js
@@ -5,14 +5,13 @@ const dFn = {
     qs : x => document.querySelector(x),
     qsa : x => document.querySelectorAll(x),
     cg : x => console.log(x),
+    addZero : x => x<10?'0'+x:x,
     // 날짜찍기 형식변경함수(yyyy-mm-dd시간)
-    fm : x =>`
-    ${x.getFullYear()}
-    -${x.getMonth()+1<10?
-        '0'+(x.getMonth()+1):x.getMonth()+1}
-    -${x.getDate()<10?
-        '0'+x.getDate():x.getDate()} 
-    (${week[x.getDay()]})`,
+    fm : x =>`${
+        x.getFullYear()
+    }-${dFn.addZero(x.getMonth()+1)
+    }-${dFn.addZero(x.getDate())
+    } (${week[x.getDay()]})`,
 }; //////////dFn 객체 ////////////////
 // 요일변경배열
 const week = ["일","월","화","수","목","금","토"];
@@ -34,14 +33,21 @@ function makeDallyeok(){
     const monthTit = dFn.qs('.monthTit');
     // (5) 날짜 요소 : .dates
     const dates = dFn.qs('.dates');
+    // (6) 날짜 넣을 배열변수
+    const dateSet = [];
 
-    dFn.cg(yearTit);
-    dFn.cg(monthTit);
-    dFn.cg(dates);
+    // dFn.cg(yearTit);
+    // dFn.cg(monthTit);
+    // dFn.cg(dates);
+    
 
     // 2. 함수 만들기 /////////////
     // (1) 달력 초기화구성 함수 ////////
     const initDallyeok = () => {
+        // 현재년
+        let cYr =currDate.getFullYear(); 
+        // 현재달
+        let cMt = currDate.getMonth();
 
         // [ 전달 끝날짜,첫날짜 알아오기 ]
         // new Date(년도,월,0-끝날짜/1-첫날짜)
@@ -52,5 +58,67 @@ function makeDallyeok(){
 
         // 1. 전달 마지막 날짜(옵션 : 0) 
         // -> 달력 처음 전달 끝 쪽 표시
+        const prevLast = new Date(cYr,cMt,0);
+        dFn.cg("전달끝날짜: "+dFn.fm(prevLast));
+
+        // 2.현재달 첫째날짜(옵션:1->전달로 셋팅)
+        // -> 달력 전달셋팅을 위한 요일 구하기 위해!
+        const thisFirst = new Date(cYr,cMt,1);
+        dFn.cg("현재달 첫째날짜: "+dFn.fm(thisFirst));
+
+        // 3. 현재달 마지막날짜(옵션: 0)
+        const thisLast = new Date(cYr,cMt+1,0);
+        dFn.cg("현재달 마지막날짜: "+dFn.fm(thisLast));
+
+        // 4. 연도 표시하기
+        yearTit.innerHTML = cYr;
+
+        // 5. 월 표시하기
+        monthTit.innerHTML = cMt+1;
+
+        // 6. 날짜 데이터 태그 구성하기
+        // 6-1. 전달 날짜 앞쪽 채우기
+        // 조건 : 현재달 첫날짜 요일이 0이 아니면 내용있음
+        // -> 왜 0인가? 0은 일요일 이므로 0이면 앞에 내용없음
+        let fDay =thisFirst.getDay();
+        dFn.cg('이번달 첫날 요일:'+fDay);
+        if(fDay != 0){
+            // 만약 요일번호가 0이 아니면 for문 돌림
+            for(let i=0;i<fDay;i++){
+                // 반복횟수만큼 배열앞쪽에 추가
+                // 앞에 추가 메서드 : unshift()
+                dateSet.unshift(`
+                    <span style="color:#ccc" class="bm">
+                        ${prevLast.getDate() - i}
+                    </span>
+                `);
+                // 전달 마지막날짜 - for문 i값(0,1,2,...)
+            }/////// for 문 ///////
+        }////////if 문////////////////////
+
+        
+        // 6-2. 현재월 삽입하기 //////////
+        // 반복문 구성 : 현재월 1일부터 마지막 날짜까지 반복 배열추가
+        for(let i = 1;i<=thisLast.getDate();i++){
+            dateSet.push(i);
+        }
+
+        // 6-3. 다음달 나머지 칸 삽입하기
+        // 다음달은 클래스 'am'으로 구분
+        // 반복구성: 1부터 2주분량 정도 넣는다
+        for(let i=1;i<=14;i++){
+            dateSet.push(`
+                <span style="color:#ccc" class="am">${i}</span>
+            `)
+        }
+   
+        dFn.cg("날짜배열 : "+dateSet);
+        // 7. 날짜배열로 날짜 태그 구성하여 출력하기
+        // 7일 * 6주 = 42개
+        dates.innerHTML = dateSet.map((v,i)=>i<42?`<div class="date">${v}</div>`:'').join('');
+
     }; //////////////// initDallyeok 함수 ////////////
+
+    // 초기셋팅함수 호출
+    initDallyeok();
 }///////////////// makeDallyeok 함수 /////////////////
