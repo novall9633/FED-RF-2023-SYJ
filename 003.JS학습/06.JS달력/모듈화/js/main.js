0c99082a898a921a88cc370b32253fef24bc32eb
nons9633 <novall9633@naver.com>
2023-10-18 14:51:11 +0900

    10/18

---
+++ 003.JS학습/06.JS달력/모듈화/js/main.js
@@ -0,0 +1,9 @@
+// 달력 컴포넌트를 부르는 메인 JS - main.js
+
+// 달력 컴포넌트 불러오기
+import DalCom from "./calendar.js";
+
+// 달력 컴포넌트 함수 호출하여 달력 셋팅하기
+// 대상 : .dalcom1 , .dalcom2
+new DalCom('.dalcom1');
+const dc2 = new DalCom('.dalcom2');
