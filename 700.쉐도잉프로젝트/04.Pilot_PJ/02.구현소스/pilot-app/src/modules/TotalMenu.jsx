9bd1faf2db8fce73e36ac6598e59f384eff7c683
nons9633 <novall9633@naver.com>
2023-11-22 14:06:24 +0900

    11/22

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/TotalMenu.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/TotalMenu.jsx
@@ -1,6 +1,21 @@
 // Pilot PJ 전체 메뉴 컴포넌트
 
+// 컨텍스트 API 불러오기
+import { useContext } from "react";
+import { pCon } from "./PilotContext";
+
 export function TotalMenu() {
+    // 컨텍스트 사용
+    const myCon = useContext(pCon);
+    // pCon에 Provider value 속성에 공개한 변수/함수를 사용함
+    
+    // 메뉴 이동 처리 함수 ////////////
+    const goPage = (txt) => {
+        myCon.chgPgName(txt);
+        // 전체박스 숨기기
+        document.querySelector('.ham').style.display = 'none';
+    } //////goPage 메서드 ////////////
+    // 리턴 코드 ////////////////////////////////////////
     return (
         <>
             <div className="mbox">
@@ -8,7 +23,7 @@ export function TotalMenu() {
                 <nav className="mlist">
                     <dl>
                         <dt>
-                            <a href="#">MEN</a>
+                            <a href="#" onClick={()=>goPage('men')}>MEN</a>
                         </dt>
                         <dd>
                             <a href="#">T-SHIRT</a>
@@ -25,7 +40,7 @@ export function TotalMenu() {
                     </dl>
                     <dl>
                         <dt>
-                            <a href="#">WOMEN</a>
+                            <a href="#" onClick={()=>goPage('women')}>WOMEN</a>
                         </dt>
                         <dd>
                             <a href="#">T-SHIRT</a>
@@ -42,7 +57,7 @@ export function TotalMenu() {
                     </dl>
                     <dl>
                         <dt>
-                            <a href="#">STYLE</a>
+                            <a href="#" onClick={()=>goPage('style')}>STYLE</a>
                         </dt>
                         <dd>
                             <a href="#">COLLECTION</a>
---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -0,0 +1,12 @@
+// 공통패션 서브페이지 컨텐츠 컴포넌트
+
+export function Fashion(props){
+    // props.cat - 서브 카테고리명
+
+    return(
+        <>
+            <h2>{PaymentResponse.cat}</h2>
+        </>
+    )
+
+} // MenSub 컴포넌트 
