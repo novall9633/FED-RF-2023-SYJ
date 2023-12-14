433ba63388c1016bda8e14cba7b1c7c1888d364f
nons9633 <novall9633@naver.com>
2023-12-14 17:18:23 +0900

    12/14

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
@@ -5,158 +5,250 @@ import { useEffect, useState } from "react";
 import "../css/cartlist.css";
 
 // 제이쿼리
-import $ from 'jquery';
+import $ from "jquery";
 import { memo } from "react";
 
 // 전달값이 변경되면 리랜더링하기 위해 메모이제이션을 적용
-export const CartList = memo(({selData,flag}) => {
-  // selData = 현재 반영된 데이터
-  // flag  = 상태값 체크변수(true/false)
-  console.log("업데이트 상태값 : ",flag.current);
+export const CartList = memo(({ selData, flag }) => {
+    // selData = 현재 반영된 데이터
+    // flag  = 상태값 체크변수(true/false)
+    console.log("업데이트 상태값 : ", flag.current);
 
-  
-  // 상태관리변수 설정 ////////////
-  // 1. 변경 데이터 변수 : 전달된 데이터로 초기 셋팅
-  const [cartData, setCartData] = useState(selData);
-  // 선택 데이터 : 로컬스토리지 데이터를 객체변환! ->주석
-  // const selData = JSON.parse(localStorage.getItem("cart"));
-  console.log("받은 데이터", selData, '\n기존 데이터',cartData);
+    // 상태관리변수 설정 ////////////
+    // 1. 변경 데이터 변수 : 전달된 데이터로 초기 셋팅
+    const [cartData, setCartData] = useState(selData);
+    // 선택 데이터 : 로컬스토리지 데이터를 객체변환! ->주석
+    // const selData = JSON.parse(localStorage.getItem("cart"));
+    console.log("받은 데이터", selData, "\n기존 데이터", cartData);
 
-  // 카트 컴포넌트의 데이터가 상태관리되고 있으므로
-  // 외부에서 전달되는 데이터와 다를 때 업데이트해야
-  // 외부에서 들어오는 데이터가 반영되어 리랜더링된다.
-  // 삭제버튼도 작동하게 하려면? -> 상태변수로 제어한다
-  // 외부데이터업데이트는 flag.current값이 true까지 되어야한다.
-  if(cartData!==selData&&flag.current) setCartData(selData);
+    // 카트 컴포넌트의 데이터가 상태관리되고 있으므로
+    // 외부에서 전달되는 데이터와 다를 때 업데이트해야
+    // 외부에서 들어오는 데이터가 반영되어 리랜더링된다.
+    // 삭제버튼도 작동하게 하려면? -> 상태변수로 제어한다
+    // 외부데이터업데이트는 flag.current값이 true까지 되어야한다.
+    if (cartData !== selData && flag.current) setCartData(selData);
 
-  // 데이터개수
-  const cntData = cartData.length;
+    // 데이터개수
+    const cntData = cartData.length;
 
-  console.log(cartData,cntData);
+    console.log(cartData, cntData);
 
-  // 전체합계 구하기
-  let totalCnt = 0;
-  cartData.forEach(v=>{
-    totalCnt += v.ginfo[3]*v.num;
-  }); ///////////////forEach /////////////
-  console.log(totalCnt);
+    // 전체합계 구하기
+    let totalCnt = 0;
+    cartData.forEach((v) => {
+        totalCnt += v.ginfo[3] * v.num;
+    }); ///////////////forEach /////////////
+    console.log(totalCnt);
 
-  //정규식함수(숫자 세자리마다 콤마해주는 기능)
-  function addComma(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+    //정규식함수(숫자 세자리마다 콤마해주는 기능)
+    function addComma(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
-  // 랜더링 후 실행 구역////////////////////
-  useEffect(()=>{
-    // 장바구니 보기 버튼 만들기
-    $('#mycart').removeClass('on').fadeIn(300,function(){ //페이드 애니 후
-      $(this).addClass('on');
-    }) /////fadeIn ////////
-    console.log("나야나");
-  },[]);/////////////useEffect ///////////////////
+    // 랜더링 후 실행 구역////////////////////
+    useEffect(() => {
+        // 장바구니 보기 버튼 만들기
+        $("#mycart")
+            .removeClass("on")
+            .fadeIn(300, function () {
+                //페이드 애니 후
+                $(this).addClass("on");
+            }); /////fadeIn ////////
+        console.log("나야나");
+    }, []); /////////////useEffect ///////////////////
 
-  // 리스트 보이기 함수 //////////////
-  const showList = () => {
-    console.log('열려라');
-    $('#cartlist').animate({right:"0"},600);
-  }; ////////////////showList //////////////////////
-  // 리스트 숨기기 함수 //////////////////
-  const hideList = (e) => {
-    e.preventDefault();
-    $('#cartlist').animate({right:'-60%'},600);
-  }
+    // 리스트 보이기 함수 //////////////
+    const showList = () => {
+        console.log("열려라");
+        $("#cartlist").animate({ right: "0" }, 600);
+    }; ////////////////showList //////////////////////
+    // 리스트 숨기기 함수 //////////////////
+    const hideList = (e) => {
+        e.preventDefault();
+        $("#cartlist").animate({ right: "-60%" }, 600);
+    };
 
-  // 리스트 삭제 함수 //////////////////
-  const deleteItem = (e) =>{
-    // 삭제 기능만 작동하기 위해 부모의 useRef값인 flag값을
-    // false로 변경하면 상단의 조건 업데이트 값이 작동하지 않는다
-    // 삭제 기능만 작동한다.
-    flag.current = false;
-    const selIdx = $(e.target).attr('data-idx');
-    console.log('지울아이:',selIdx);
+    // 리스트 삭제 함수 //////////////////
+    const deleteItem = (e) => {
+        // 삭제 기능만 작동하기 위해 부모의 useRef값인 flag값을
+        // false로 변경하면 상단의 조건 업데이트 값이 작동하지 않는다
+        // 삭제 기능만 작동한다.
+        flag.current = false;
 
-    // 해당 데이터 순번 알아내기
-    const newData = cartData.filter((v)=>{
-      if(v.idx!==selIdx) return true; 
-    });
+        let confMsg = "해당 제품을 장바구니에서 지우시겠습니까?";
+        // 지울지 여부를 사용자에게 물어본다
+        // confirm() 대화창에 '확인' -> true, '취소'->false 리턴함
+        // confirm은 alert과 유사하게 window객체에 있음
+        if (window.confirm(confMsg)) {
+            const selIdx = $(e.target).attr("data-idx");
+            console.log("지울아이:", selIdx);
 
-    console.log('제거후 리스트:',newData);
+            // 해당 데이터 순번 알아내기
+            const newData = cartData.filter((v) => {
+                if (v.idx !== selIdx) return true;
+            });
 
-    // 로컬스 데이터 업데이트
-    localStorage.setItem('cart',JSON.stringify(newData));
+            console.log("제거후 리스트:", newData);
 
-    // 전체 데이터 업데이트 하면 모두 리랜더링 되게 하자
-    setCartData(newData);
-  }; ////////////////deleteItem 함수 ////////////////////
+            // 로컬스 데이터 업데이트
+            localStorage.setItem("cart", JSON.stringify(newData));
 
-  // 리턴 코드 ////////////////////////////////////////////////
-  return (
-    <>
-      <section id="cartlist">
-        <a href="#" className="cbtn cbtn2" onClick={hideList}>
-          <span>닫기버튼</span>
-        </a>
-        <table>
-          <caption>
-            <h1> 카트 리스트</h1>
-          </caption>
-          <tbody>
-            <tr>
-              <th>상품</th>
-              <th>번호</th>
-              <th>상품명</th>
-              <th>상품코드</th>
-              <th>단가</th>
-              <th>수량</th>
-              <th>합계</th>
-              <th>삭제</th>
-            </tr>
+            // 전체 데이터 업데이트 하면 모두 리랜더링 되게 하자
+            setCartData(newData);
+        } //////////////////////// if /////////////////////
+    }; ////////////////deleteItem 함수 ////////////////////
 
-            {cartData.map((v, i) => (
-              <tr key={i}>
-                {/* 상품이미지 */}
-                <td>
-                  <img
-                    src={"images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"}
-                    alt="item"
-                  />
-                </td>
-                {/* 리스트순번 */}
-                <td>{i + 1}</td>
-                {/* 상품명 */}
-                <td>{v.ginfo[1]}</td>
-                {/* 상품코드 */}
-                <td>{v.ginfo[2]}</td>
-                {/* 상품가격 */}
-                <td>{addComma(v.ginfo[3])}원</td>
-                {/* 상품수량 */}
-                <td>{v.num}</td>
-                {/* 상품가격 총합계 */}
-                <td>{addComma(v.ginfo[3] * v.num)}원</td>
-                {/* 삭제버튼 */}
-                <td>
-                  <button className="cfn" data-idx={v.idx} onClick={deleteItem}>
-                    ×
-                  </button>
-                </td>
-              </tr>
-            ))}
+    // 증감 반영 함수 ////////////////
+    const chgNum = (e) => {
+        // 0-감소, 1-증가
+        // 이벤트 타겟
+        const tg = $(e.currentTarget);
+        // 이벤트 타겟의 입력창
+        const tgInput = tg.parent().siblings(".item-cnt");
+        // 입력창 숫자 읽기 : 문자형 숫자 -> 숫자형
+        let cNum = Number(tgInput.val());
 
-            <tr>
-              <td colSpan="6">총합계 :</td>
-              <td>{addComma(totalCnt)}원</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-      {/* 카트버튼이미지 박스 */}
-      <div id="mycart" onClick={showList}>
-        {/* 카트이미지 */}
-        <img src="./images/mycart.gif" title="개의 상품이 있습니다." />
-        {/* 카트상품개수 출력박스 */}
-        <div className="cntBx">{cntData}</div>
-      </div>
-    </>
-  );
+        console.log("증감반영:");
+        // CSS 포커스시 반영버튼 보이기 셋팅에 맞춰서
+        // 강제로 입력창에 포커스 주기
+        tgInput.focus();
+
+        // 증감 하기
+        if (tg.attr("alt") === "증가") {
+            cNum++;
+        } else {
+            cNum--;
+        }
+        // 한계수 체크
+        if (cNum < 1) {
+            cNum = 1;
+        }
+
+        // 화면 반영하기
+        tgInput.val(cNum);
+    }; ///////////////chgNum 함수 ////////////////
+
+    // 반영버튼 클릭시 데이터 업데이트 하기 //////
+    const goResult = (e) => {
+      // 업데이트할 배열 고유값 idx
+      let tg = $(e.currentTarget);
+      let cidx = $(e.currentTarget).attr('data-idx');
+        console.log("결과야 나와라~!",cidx);
+
+        // 데이터 리랜더링 중복실행 막기
+        flag.current = false;
+
+        // 해당 데이터 업데이트하기
+        // forEach로 돌리면 중간에 맞을 경우 return 할 수 없음
+        // 일반 for문으로 return 또는 continue사용 가능
+
+        // ->>>>some()이라는 메서드가 있다.
+        // return true로 조건에 처리시 for문을 빠져나옴(return과 유사)
+        // return false로 조건 처리시 for문을 해당순번 제외하고 계속 순회함(continue와 유사)
+        // 참고 : https://www.w3schools.com/jsref/jsref_some.asp
+
+        // Array some() 메서드 테스트
+        // cartData.some((v) => {
+        //   console.log("some test 상단:",v.idx);
+        //   // if(v.idx==17){return true;} // -> for문 break 유사
+        //   if(v.idx==17){return false;} // -> for문 continue 유사
+        //   console.log("some test 하단:",v.idx);
+        // });
+
+        // 클릭 시 'data-idx'값에 업데이트 할 요소 idx번호 있음 -> cidx
+        cartData.some((v,i) => {
+          // 해당 순번 업데이트 하기
+          if(v.idx==cidx){
+            // 업데이트 하기
+            cartData[i].num = tg.prev().val();
+            // some 메서드 이므로 true 리턴시 순회 종료
+            return true;
+          }/////////if ////////
+        });
+        // 로컬스 데이터 업데이트
+        localStorage.setItem("cart", JSON.stringify(cartData));
+        // 전체 데이터 업데이트 하면 모두 리랜더링 되게 하자
+        setCartData(cartData);
+    }; //////////////goResult 함수 ///////////////////////
+
+    // 리턴 코드 ////////////////////////////////////////////////
+    return (
+        <>
+            <section id="cartlist">
+                <a href="#" className="cbtn cbtn2" onClick={hideList}>
+                    <span>닫기버튼</span>
+                </a>
+                <table>
+                    <caption>
+                        <h1> 카트 리스트</h1>
+                    </caption>
+                    <tbody>
+                        <tr>
+                            <th>상품</th>
+                            <th>번호</th>
+                            <th>상품명</th>
+                            <th>상품코드</th>
+                            <th>단가</th>
+                            <th>수량</th>
+                            <th>합계</th>
+                            <th>삭제</th>
+                        </tr>
+
+                        {cartData.map((v, i) => (
+                            <tr key={i}>
+                                {/* 상품이미지 */}
+                                <td>
+                                    <img src={"images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"} alt="item" />
+                                </td>
+                                {/* 리스트순번 */}
+                                <td>{i + 1}</td>
+                                {/* 상품명 */}
+                                <td>{v.ginfo[1]}</td>
+                                {/* 상품코드 */}
+                                <td>{v.ginfo[2]}</td>
+                                {/* 상품가격 */}
+                                <td>{addComma(v.ginfo[3])}원</td>
+                                {/* 상품수량 */}
+                                <td className="cnt-part">
+                                    <div>
+                                        <span>
+                                            <input type="text" className="item-cnt" defaultValue={v.num} />
+                                            <button className="btn-insert" onClick={goResult} data-idx={v.idx}>
+                                                반영
+                                            </button>
+                                            <b className="btn-cnt">
+                                                <img src="./images/cnt_up.png" alt="증가" onClick={chgNum} />
+                                                <img src="./images/cnt_down.png" alt="감소" onClick={chgNum} />
+                                            </b>
+                                        </span>
+                                    </div>
+                                </td>
+                                {/* 상품가격 총합계 */}
+                                <td>{addComma(v.ginfo[3] * v.num)}원</td>
+                                {/* 삭제버튼 */}
+                                <td>
+                                    <button className="cfn" data-idx={v.idx} onClick={deleteItem}>
+                                        ×
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+
+                        <tr>
+                            <td colSpan="6">총합계 :</td>
+                            <td>{addComma(totalCnt)}원</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+            {/* 카트버튼이미지 박스 */}
+            <div id="mycart" onClick={showList}>
+                {/* 카트이미지 */}
+                <img src="./images/mycart.gif" title="개의 상품이 있습니다." />
+                {/* 카트상품개수 출력박스 */}
+                <div className="cntBx">{cntData}</div>
+            </div>
+        </>
+    );
 }); ////////////// CartList 컴포넌트 /////////
